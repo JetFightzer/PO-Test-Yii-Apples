b336e9658cc8f6c92e5b8d3ade505164c250cba3
JetFightzer <jetfightzer@gmail.com>
2020-12-07 13:39:17 +0300

    Add Apple controller and view

---
+++ backend/views/apple/index.php
@@ -0,0 +1,61 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Apples';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="apple-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Apple', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],   
+
+            'id',
+            [
+                'attribute' => 'color', 
+                'format' => 'raw',
+                'value' => function($model) {
+                    $options1 = $options2 = [
+                        'style' => [
+                            'padding' => '0 4px',
+                            'background' => $model->color,
+                            'font-family' => 'monospace',
+                        ],
+                    ];
+                    Html::addCssStyle($options1, ['color' => 'black']);
+                    Html::addCssStyle($options2, ['color' => 'white']);
+                    return
+                        Html::tag('span', $model->color, $options1) .
+                        Html::tag('span', $model->color, $options2);
+                }
+            ],
+            'appearance_date',
+            'fall_date',
+            [
+                'header' => 'State',
+                'class'  => 'yii\grid\DataColumn', // can be omitted, as it is the default
+                'value'  => function ($data) {
+                    return $data->stateLabeled; // $data['name'] for array data, e.g. using SqlDataProvider.
+                },
+            ],
+            'left',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
---
+++ backend/views/apple/view.php
@@ -0,0 +1,69 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Apple */
+
+$this->title = $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Apples', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+\yii\web\YiiAsset::register($this);
+?>
+<div class="apple-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            [
+                'attribute' => 'color', 
+                'format' => 'raw',
+                'value' => function($model) {
+                    $options1 = $options2 = [
+                        'style' => [
+                            'padding' => '0 4px',
+                            'background' => $model->color,
+                            'font-family' => 'monospace',
+                        ],
+                    ];
+                    Html::addCssStyle($options1, ['color' => 'black']);
+                    Html::addCssStyle($options2, ['color' => 'white']);
+                    return
+                        Html::tag('span', $model->color, $options1) .
+                        Html::tag('span', $model->color, $options2);
+                }
+            ],
+            'appearance_date',
+            'fall_date',
+            [
+                'header' => 'State',
+                'class'  => 'yii\grid\DataColumn', // can be omitted, as it is the default
+                'value'  => function ($data) {
+                    return $data->stateLabeled; // $data['name'] for array data, e.g. using SqlDataProvider.
+                },
+            ],
+            [
+                'attribute' => 'left',
+                'value' => function($model) {
+                    return $model->left . '%';
+                }
+            ],
+        ],
+    ]) ?>
+
+</div>
